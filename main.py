a41cac23f8ebedee6292c8b079b6ffb3138bd104
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-04-29 13:13:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,129 @@ FayasNoushad = Client(
     api_hash = os.environ["API_HASH"],
 )
 
+START_TEXT = """
+Hello {}, I am a country information finder bot. Give me a country name I will send the informations of the country.
+
+Made by @FayasNoushad
+"""
+HELP_TEXT = """
+- Just send me a country name
+- Then I will check and send you the informations
+
+<b><u>Informations :-</u></b>
+Name, Native Name, Capital, Population, Region, Sub Region, Top Level Domains, Calling Codes, Currencies, Residence, Timezone, Wikipedia, Google
+
+Made by @FayasNoushad
+"""
+ABOUT_TEXT = """
+- **Bot :** `Country Info Bot`
+- **Creator :** [Fayas](https://telegram.me/TheFayas)
+- **Channel :** [Fayas Noushad](https://telegram.me/FayasNoushad)
+- **Source :** [Click here](https://github.com/FayasNoushad/Country-Info-Bot/tree/main)
+- **Language :** [Python3](https://python.org)
+- **Library :** [Pyrogram](https://pyrogram.org)
+- **Server :** [Heroku](https://heroku.com)
+"""
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
+        InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
+        ],[
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+ERROR_BUTTON = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+
+@FayasNoushad.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            reply_markup=START_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            reply_markup=HELP_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_TEXT,
+            reply_markup=ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
+    else:
+        await update.message.delete()
+
+@FayasNoushad.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=START_BUTTONS
+    )
+
+@FayasNoushad.on_message(filters.private & filters.text)
+async def countryinfo(bot, update):
+    country = CountryInfo(update.text)
+    info = f"""
+Name : `{country.name()}`
+Native Name : `{country.native_name()}`
+Capital : `{country.capital()}`
+Population : `{country.population()}`
+Region : `{country.region()}`
+Sub Region : `{country.subregion()}`
+Top Level Domains : `{country.tld()}`
+Calling Codes : `{country.calling_codes()}`
+Currencies : `{country.currencies()}`
+Residence : `{country.demonym()}`
+Timezone : `{country.timezones()}`
+"""
+    reply_markup=InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Wikipedia', url=f'{country.wiki()}'),
+        InlineKeyboardButton('Google', url=f'https://www.google.com/search?q={country.name()}')
+        ],[
+        InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
+        InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
+
+        ]]
+    )
+    try:
+        await update.reply_text(
+            text=info,
+            reply_markup=reply_markup,
+            disable_web_page_preview=True
+        )
+    except FloodWait as floodwait:
+        await asyncio.sleep(floodwait.x)
+        return countryinfo(bot, update)
+    except KeyError as keyerror:
+        print(keyerror)
+    except Exception as error:
+        print(error)
+
 FayasNoushad.run()
